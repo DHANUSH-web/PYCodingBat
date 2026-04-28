3fce7dcb9d7e084c67f165843306b4ce7261f887
Dhanush H V <dhanushhv75@gmail.com>
2026-04-28 20:24:26 +0000

    Codingbat: Python

---
+++ main.py
@@ -0,0 +1,245 @@
+from dataclasses import dataclass
+import requests
+import unittest
+
+
+def concat(a: str, b: str) -> str:
+    a_size: int = len(a)
+    b_size: int = len(b)
+
+    if a_size == 0:
+        return b
+    if b_size == 0:
+        return a
+
+    return a + b[1:] if a[-1] == b[0] else a + b
+
+
+def last_two(data: str) -> str:
+    if len(data) < 2:
+        return ""
+    return data[:-2] + data[:-3:-1] if len(data) > 2 else data[::-1]
+
+
+def see_colors(data: str) -> str:
+    if data.startswith("red"):
+        return "red"
+    elif data.startswith("blue"):
+        return "blue"
+
+    return ""
+
+
+def front_again(data: str) -> bool:
+    size: int = len(data)
+    return size >= 2 and data[0] == data[size-2] and data[1] == data[size-1]
+
+
+def min_cat(a: str, b: str) -> str:
+    a_size = len(a)
+    b_size = len(b)
+
+    if a_size == b_size:
+        return a + b
+
+    return a[a_size-b_size:] + b if a_size > b_size else a + b[b_size-a_size:]
+
+
+def extra_front(data: str) -> str:
+    return data * 3 if len(data) < 2 else data[0:2]*3
+
+
+def near10(num: int) -> bool:
+    return num % 10 <= 2 or num % 10 >= 8
+
+
+def tea_party(tea: int, candy: int) -> int:
+    if tea < 5 or candy < 5:
+        return 0
+    elif tea >= candy*2 or candy >= tea*2:
+        return 2
+    return 1
+
+
+def fizz_string(data: str) -> str:
+    if data.startswith('f') and data.endswith('b'):
+        return "FizzBuzz"
+    elif data.startswith('f'):
+        return "Fizz"
+    elif data.endswith('b'):
+        return "Buzz"
+    return data
+
+
+def fizz_string2(n: int) -> str:
+    if not n % 3 and not n % 5:
+        return "FizzBuzz!"
+    if not n % 3:
+        return "Fizz!"
+    if not n % 5:
+        return "Buzz!"
+    
+    return f"{n}!"
+
+
+def two_as_one(a: int, b: int, c: int) -> bool:
+    return a + b == c or a + c == b or b + c == a
+
+
+def in_order(a: int, b: int, c: int, b_ok: bool) -> bool:
+    return (b_ok or b > a) and c > b
+
+
+def in_order_equal(a: int, b: int, c: int, equal_ok: bool) -> bool:
+    return (equal_ok and a <= b <= c) or (a < b < c)
+
+
+@dataclass
+class JsonDataProps:
+    userId: int
+    id: int
+    title: str
+    completed: bool
+
+
+def fetch_json_data(url: str, params: dict | None = None) -> JsonDataProps:
+    response: requests.Response = requests.get(url=url, params=params)
+
+    if response.status_code != 200:
+        raise requests.HTTPError(f"Failed to fetch {url}, status code: {response.status_code}")
+    
+    data: dict = response.json()
+    return JsonDataProps(
+        userId=data['userId'],
+        id=data['id'],
+        title=data['title'],
+        completed=data['completed'],
+    )
+
+
+def lastDigit(a: int, b: int, c: int) -> bool:
+    return a % 10 == b % 10 or a % 10 == c % 10 or b % 10 == c % 10
+
+
+def lessBy10(a: int, b: int, c: int) -> bool:
+    return abs(a - b) >= 10 or abs(a - c) >= 10 or abs(b - c) >= 10
+
+
+def withoutDoubles(die1: int, die2: int, no_doubles: bool) -> int:
+    if no_doubles and die1 == die2:
+        die1 = 1 if die1 == 6 else die1 + 1
+    return die1 + die2
+
+
+def maxMod5(a: int, b: int) -> int:
+    if a == b:
+        return 0
+    
+    if a % 5 == b % 5:
+        return min(a, b)
+    
+    return max(a, b)
+
+
+def redTicket(a: int, b: int, c: int) -> int:
+    if a == b == c == 2:
+        return 10
+    
+    if a == b == c:
+        return 5
+    
+    if a != b and a != c:
+        return 1
+    
+    return 0
+
+
+def greenTicket(a: int, b: int, c: int) -> int:
+    if a == b == c:
+        return 20
+    
+    if a == b or a == c or b == c:
+        return 10
+    
+    return 0
+
+
+def shareDigit(a: int, b: int) -> bool:
+    return a // 10 == b // 10 or a // 10 == b % 10 or a % 10 == b // 10 or a % 10 == b % 10
+
+
+def fact(n: int) -> int:
+    return 1 if n <= 1 else n * fact(n - 1)
+
+
+def sum13(nums: list[int]) -> int:
+    if len(nums) == 0:
+        return 0
+    
+    if nums[0] == 13:
+        return 0
+
+    return nums[0] + sum13(nums[1:])
+
+
+def centeredAverage(nums: list[int]) -> int:
+    sum, _min, _max = 0, nums[0], nums[0]
+    
+    for i in range(len(nums)):
+        sum += nums[i]
+        
+        if nums[i] < _min:
+            _min = nums[i]
+        
+        if nums[i] > _max:
+            _max = nums[i]
+
+    return (sum - _min - _max) // (len(nums) - 2)
+
+
+def sum67(nums: list[int]) -> int:
+    total = 0
+    skip = False
+
+    for n in nums:
+        if n == 6:
+            skip = True
+        elif n == 7 and skip:
+            skip = False
+        elif not skip:
+            total += n
+    
+    return total
+
+
+def has22(nums: list[int]) -> bool:
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1] == 2:
+            return True
+    return False
+
+
+def lucky13(nums: list[int]) -> bool:
+    for n in nums:
+        if n == 1 or n == 3:
+            return False
+    return True
+
+
+def sum28(nums: list[int]) -> bool:
+    total = 0
+
+    for n in nums:
+        if n == 2:
+            total += n
+    
+    return total == 8
+
+
+if __name__ == "__main__":
+    # print("Run test cases with `uv run python unittest -v`")
+    print("Running unit tests...")
+    # os.system("uv run python -m unittest -v")
+    tests = unittest.TestLoader().discover(start_dir="tests", pattern="test_*.py")
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(tests)
---
+++ tests/test_main.py
@@ -0,0 +1,151 @@
+import unittest
+from main import *
+
+class TestMain(unittest.TestCase):
+
+    def test_concat(self):
+        self.assertEqual(concat("abc", "cat"), "abcat")
+        self.assertEqual(concat("dog", "cat"), "dogcat")
+        self.assertEqual(concat("abc", ""), "abc")
+    
+    def test_lastTwo(self):
+        self.assertEqual(last_two("coding"), "codign")
+        self.assertEqual(last_two("cat"), "cta")
+        self.assertEqual(last_two("ab"), "ba")
+    
+    def test_seeColors(self):
+        self.assertEqual(see_colors("redxx"), "red")
+        self.assertEqual(see_colors("xxred"), "")
+        self.assertEqual(see_colors("blueTimes"), "blue")
+    
+    def test_frontAgain(self):
+        self.assertTrue(front_again("edited"))
+        self.assertFalse(front_again("edit"))
+        self.assertTrue(front_again("ed"))
+
+    def test_minCat(self):
+        self.assertEqual(min_cat("Hello", "Hi"), "loHi")
+        self.assertEqual(min_cat("Hello", "java"), "ellojava")
+        self.assertEqual(min_cat("java", "Hello"), "javaello")
+    
+    def test_extraFront(self):
+        self.assertEqual(extra_front("Hello"), "HeHeHe")
+        self.assertEqual(extra_front("ab"), "ababab")
+        self.assertEqual(extra_front("H"), "HHH")
+
+    def test_near10(self):
+        self.assertTrue(near10(12))
+        self.assertFalse(near10(17))
+        self.assertTrue(near10(19))
+
+    def test_teaParty(self):
+        self.assertEqual(tea_party(6, 8), 1)
+        self.assertEqual(tea_party(3, 8), 0)
+        self.assertEqual(tea_party(20, 6), 2)
+
+    def test_fizzString(self):
+        self.assertEqual(fizz_string("fig"), "Fizz")
+        self.assertEqual(fizz_string("dib"), "Buzz")
+        self.assertEqual(fizz_string("fib"), "FizzBuzz")
+        self.assertEqual(fizz_string("none"), "none")
+
+    def test_fizzString2(self):
+        self.assertEqual(fizz_string2(1), "1!")
+        self.assertEqual(fizz_string2(2), "2!")
+        self.assertEqual(fizz_string2(3), "Fizz!")
+        self.assertEqual(fizz_string2(5), "Buzz!")
+        self.assertEqual(fizz_string2(15), "FizzBuzz!")
+
+    def test_twoAsOne(self):
+        self.assertTrue(two_as_one(1, 2, 3))
+        self.assertTrue(two_as_one(3, 1, 2))
+        self.assertFalse(two_as_one(3, 2, 2))
+
+    def test_inOrder(self):
+        self.assertTrue(in_order(1, 2, 4, False))
+        self.assertFalse(in_order(1, 2, 1, False))
+        self.assertTrue(in_order(1, 1, 2, True))
+
+    def test_inOrderEqual(self):
+        self.assertTrue(in_order_equal(2, 5, 11, False), True)
+        self.assertFalse(in_order_equal(5, 7, 6, False), False)
+        self.assertTrue(in_order_equal(5, 5, 7, True), True)
+
+    def test_fetchJsonResponse(self):
+        url: str = "https://jsonplaceholder.typicode.com/todos/1"
+        data: JsonDataProps = fetch_json_data(url=url)
+
+        self.assertEqual(data.userId, 1)
+        self.assertEqual(data.id, 1)
+        self.assertEqual(data.title, "delectus aut autem")
+        self.assertFalse(data.completed)
+
+    def test_lastDigit(self):
+        self.assertTrue(lastDigit(23, 19, 13))
+        self.assertFalse(lastDigit(23, 19, 20))
+        self.assertTrue(lastDigit(23, 19, 3))
+
+    def test_lessBy10(self):
+        self.assertTrue(lessBy10(1, 2, 11))
+        self.assertFalse(lessBy10(3, 2, 11))
+        self.assertTrue(lessBy10(10, 1, 11))
+
+    def test_withoutDoubles(self):
+        self.assertEqual(withoutDoubles(2, 3, True), 5)
+        self.assertEqual(withoutDoubles(3, 3, True), 7)
+        self.assertEqual(withoutDoubles(3, 3, False), 6)
+
+    def test_maxMod5(self):
+        self.assertEqual(maxMod5(2, 3), 3)
+        self.assertEqual(maxMod5(6, 2), 6)
+        self.assertEqual(maxMod5(3, 2), 3)
+
+    def test_redTicket(self):
+        self.assertEqual(redTicket(2, 2, 2), 10)
+        self.assertEqual(redTicket(2, 2, 1), 0)
+        self.assertEqual(redTicket(0, 0, 0), 5)
+
+    def test_greenTicket(self):
+        self.assertEqual(greenTicket(1, 2, 3), 0)
+        self.assertEqual(greenTicket(2, 2, 2), 20)
+        self.assertEqual(greenTicket(1, 1, 2), 10)
+
+    def test_shareDigit(self):
+        self.assertTrue(shareDigit(12, 23))
+        self.assertFalse(shareDigit(12, 43))
+        self.assertFalse(shareDigit(12, 44))
+
+    def test_sum13(self):
+        self.assertEqual(sum13([1, 2, 2, 1]), 6)
+        self.assertEqual(sum13([1, 1]), 2)
+        self.assertEqual(sum13([1, 2, 2, 1, 13]), 6)
+        
+    def test_centeredAverage(self):
+        self.assertEqual(centeredAverage([1, 2, 3, 4, 100]), 3)
+        self.assertEqual(centeredAverage([1, 1, 5, 5, 10, 8, 7]), 5)
+        self.assertEqual(centeredAverage([-10, -4, -2, -4, -2, 0]), -3)
+    
+    def test_sum67(self):
+        self.assertEqual(sum67([1, 2, 2]), 5)
+        self.assertEqual(sum67([1, 2, 2, 6, 99, 99, 7]), 5)
+        self.assertEqual(sum67([1, 1, 6, 7, 2]), 4)
+        self.assertEqual(sum67([6, 7, 2]), 2)
+        self.assertEqual(sum67([1, 6, 7, 6, 7]), 1)
+        self.assertEqual(sum67([1, 6, 7, 2, 6, 99, 7]), 3)
+        self.assertEqual(sum67([1, 6, 99, 7, 7]), 8)
+        self.assertEqual(sum67([6, 7, 7]), 7)
+    
+    def test_has22(self):
+        self.assertTrue(has22([1, 2, 2]))
+        self.assertFalse(has22([1, 2, 1, 2]))
+        self.assertFalse(has22([1, 1, 2]))
+
+    def test_lucky13(self):
+        self.assertTrue(lucky13([0, 2, 4]))
+        self.assertFalse(lucky13([1, 2, 3]))
+        self.assertFalse(lucky13([1, 2, 4]))
+
+    def test_sum28(self):
+        self.assertTrue(sum28([2, 3, 2, 2, 4, 2]))
+        self.assertFalse(sum28([2, 3, 2, 2, 4, 2, 2]))
+        self.assertFalse(sum28([1, 2, 3, 4]))
